e0494189d9502d8aaeb6ea7181e794815f6143fb
Akula Sheshu <121094754+Shesh009@users.noreply.github.com>
2024-11-27 00:12:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -293,7 +293,6 @@ def delete_account():
             cursor.close()
             connection.close()
         return render_template('delete_acc.html')
-        return redirect("/")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True,port=5000)
